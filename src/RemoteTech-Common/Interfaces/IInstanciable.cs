4044c6680cecc1564e09aaad1032e8d22266c304
neitsa <neitsa@gmail.com>
2016-12-17 19:42:17 +0100

    Squashed 'src/RemoteTech-Common/' changes from 0875b34..041018e
    041018e Save project.
    67f81cb Add more reflection functions to find Interfaces in other RemoteTech modules.
    bcf4158 Add class inheriting from CommNetVessel.
    e19791c Add delay manager and instance interfaces.
    
    git-subtree-dir: src/RemoteTech-Common
    git-subtree-split: 041018e79addd122fec8b8c1ee5e0a4ca65f92d1

---
+++ src/RemoteTech-Common/Interfaces/IInstanciable.cs
@@ -0,0 +1,7 @@
+﻿namespace RemoteTech.Common.Interfaces
+{
+    public interface IInstanciable<T>  where T : class
+    {
+        T GetInstance();
+    }
+}
---
+++ src/RemoteTech-Common/RemoteTechCommNet/RemoteTechCommNetVessel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CommNet;
+using RemoteTech.Common.Api;
+using RemoteTech.Common.Interfaces;
+
+namespace RemoteTech.Common.RemoteTechCommNet
+{
+    public class RemoteTechCommNetVessel : CommNetVessel
+    {
+        private IDelayManager _delayManager;
+
+        public override void OnNetworkPostUpdate()
+        {
+            base.OnNetworkPostUpdate();
+
+            UpdateDelay();
+        }
+
+        public virtual void UpdateDelay()
+        {
+            if (!RemoteTechModules.RemoteTechDelayAssemblyLoaded)
+                return;
+
+            if (_delayManager == null)
+                _delayManager =
+                    RemoteTechModules.GetObjectFromInterface<IDelayManager>(
+                        RemoteTechModules.RemoteTechDelayAssemblyName, Type.EmptyTypes);
+
+            // set up the delay
+            signalDelay = _delayManager?.GetVesselDelay(vessel) ?? 0;
+        }
+    }
+}
