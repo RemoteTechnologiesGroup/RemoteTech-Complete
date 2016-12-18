9550e7a25f9470c4034632205a7d81685edd87e5
neitsa <neitsa@gmail.com>
2016-12-18 16:54:39 +0100

    Add script to update subtrees.

---
+++ scripts/update_subtrees.sh
@@ -0,0 +1,5 @@
+# update subtrees
+git subtree pull --prefix=src/RemoteTech-Antennas RemoteTech-Antennas develop --squash
+git subtree pull --prefix=src/RemoteTech-Common RemoteTech-Common develop --squash
+git subtree pull --prefix=src/RemoteTech-Delay RemoteTech-Delay develop --squash
+git subtree pull --prefix=src/RemoteTech-Transmitter RemoteTech-Transmitter develop --squash
