a2f730ca39962f18af479b44d16a39c37d4c925c
neitsa <neitsa@gmail.com>
2016-12-26 17:32:59 +0100

    Update script for subtree updating: Add RT-CommandHandler.

--- scripts/update_subtrees.sh
+++ scripts/update_subtrees.sh
@@ -1,5 +1,6 @@
 # update subtrees
 git subtree pull --prefix=src/RemoteTech-Antennas RemoteTech-Antennas develop --squash
+git subtree pull --prefix=src/RemoteTech-CommandHandler RemoteTech-CommandHandler develop --squash
 git subtree pull --prefix=src/RemoteTech-Common RemoteTech-Common develop --squash
 git subtree pull --prefix=src/RemoteTech-Delay RemoteTech-Delay develop --squash
 git subtree pull --prefix=src/RemoteTech-Transmitter RemoteTech-Transmitter develop --squash
