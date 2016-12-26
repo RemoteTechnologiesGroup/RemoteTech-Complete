cb52baac15af00d224672d9289edce24d38d00ea
neitsa <neitsa@gmail.com>
2016-12-26 01:49:28 +0100

    Remove forced output directory from build.

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,7 +1,6 @@
 echo off
 rem ensure msbuild is in your path (call vcvarsall.bat or use this file from VS developer prompt or MSBuild prompt)
 set SOURCE_DIR=%~dp0\..\src
-set OUTPUT_DIR=%SOURCE_DIR%\output
 cd %SOURCE_DIR%
-msbuild RemoteTech-Complete.sln /target:Clean /p:PreBuildEvent="" /p:PostBuildEvent="" /p:Configuration=Release /p:OutputPath=%OUTPUT_DIR% /target:Build
+msbuild RemoteTech-Complete.sln /target:Clean /p:PreBuildEvent="" /p:PostBuildEvent="" /p:Configuration=Release /target:Build
 cd %~dp0
