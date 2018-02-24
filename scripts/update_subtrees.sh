580e4bc251b5a7ef6ff354cab2686ec57c52abc2
KSP-TaxiService <chinglihlim@gmail.com>
2018-02-24 21:37:19 +0800

    Switch to full repository urls in subtree pull script

--- scripts/update_subtrees.sh
+++ scripts/update_subtrees.sh
@@ -1,6 +1,6 @@
 # update subtrees
-git subtree pull --prefix=src/RemoteTech-Antennas RemoteTech-Antennas develop --squash
-git subtree pull --prefix=src/RemoteTech-CommandHandler RemoteTech-CommandHandler develop --squash
-git subtree pull --prefix=src/RemoteTech-Common RemoteTech-Common develop --squash
-git subtree pull --prefix=src/RemoteTech-Delay RemoteTech-Delay develop --squash
-git subtree pull --prefix=src/RemoteTech-Transmitter RemoteTech-Transmitter develop --squash
+git subtree pull --prefix=src/RemoteTech-Antennas https://github.com/RemoteTechnologiesGroup/RemoteTech-Antennas develop --squash
+git subtree pull --prefix=src/RemoteTech-CommandHandler https://github.com/RemoteTechnologiesGroup/RemoteTech-CommandHandler develop --squash
+git subtree pull --prefix=src/RemoteTech-Common https://github.com/RemoteTechnologiesGroup/RemoteTech-Common develop --squash
+git subtree pull --prefix=src/RemoteTech-Delay https://github.com/RemoteTechnologiesGroup/RemoteTech-Delay develop --squash
+git subtree pull --prefix=src/RemoteTech-Transmitter https://github.com/RemoteTechnologiesGroup/RemoteTech-Transmitter develop --squash
