64f598d13f59ab56a4700d0989f7f433def3c600
KSP-TaxiService <chinglihlim@gmail.com>
2018-03-03 00:11:20 +0800

    Update RT Delay Url

--- scripts/update_subtrees.sh
+++ scripts/update_subtrees.sh
@@ -2,5 +2,5 @@
 git subtree pull --prefix=src/RemoteTech-Antennas https://github.com/RemoteTechnologiesGroup/RemoteTech-Antennas develop --squash
 git subtree pull --prefix=src/RemoteTech-CommandHandler https://github.com/RemoteTechnologiesGroup/RemoteTech-CommandHandler develop --squash
 git subtree pull --prefix=src/RemoteTech-Common https://github.com/RemoteTechnologiesGroup/RemoteTech-Common develop --squash
-git subtree pull --prefix=src/RemoteTech-Delay https://github.com/RemoteTechnologiesGroup/RemoteTech-Delay develop --squash
+git subtree pull --prefix=src/RemoteTech-Delay https://github.com/RemoteTechnologiesGroup/RemoteTech-Delay taxiservice-attempt --squash
 git subtree pull --prefix=src/RemoteTech-Transmitter https://github.com/RemoteTechnologiesGroup/RemoteTech-Transmitter develop --squash
